b0b8e0350354d721b4665d161cc594ac51467a25
Diana Kim <dianajkim@gmail.com>
2019-11-24 23:49:39 -0800

    authors formatting

--- schema.sql
+++ schema.sql
@@ -13,4 +13,5 @@ CREATE TABLE books (
 INSERT INTO books (image, title, authors, description, shelf, isbn)
 VALUES ('https://books.google.com/books?id=zyTCAlFPjgYC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api','The Google story','{"David A. Vise","Mark Malseed"}','Here is the story behind one of the most remarkable Internet
   successes of our time. Based on scrupulous research and extraordinary access
-  to Google, ..."', 'Business & Economics / Entrepreneurship', '9780553804577');
+  to Google, ..."', 'Business & Economics / Entrepreneurship', '9780553804577'),
+  ;
