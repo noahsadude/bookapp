73029bd7148c345310c9787564f4832f9913cba0
Diana Kim <dianajkim@gmail.com>
2019-11-24 23:05:10 -0800

    altered authors datatype from text to text array in schema

--- schema.sql
+++ schema.sql
@@ -4,10 +4,13 @@ CREATE TABLE books (
   id SERIAL PRIMARY KEY,
   image text,
   title text,
-  authors text,
+  authors text[],
   description text,
-  shelf text
+  shelf text,
+  isbn text
 );
 
-INSERT INTO books (image, title, authors, description, shelf)
-VALUES ('http://placehold.it/128x200','test-title','test-authors','test_description','Top Shelf');
+INSERT INTO books (image, title, authors, description, shelf, isbn)
+VALUES ('https://books.google.com/books?id=zyTCAlFPjgYC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api','The Google story','{"David A. Vise","Mark Malseed"}','Here is the story behind one of the most remarkable Internet
+  successes of our time. Based on scrupulous research and extraordinary access
+  to Google, ..."', 'Business & Economics / Entrepreneurship', '9780553804577');
